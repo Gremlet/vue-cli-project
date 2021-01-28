aaadd4b0e4a039246115690938378478d410eb46
Gremlet <57172450+Gremlet@users.noreply.github.com>
2021-01-28 22:37:09 +0100

    Develop Drinks component with form and GET

--- src/router/index.js
+++ src/router/index.js
@@ -13,11 +13,8 @@ const routes = [
   {
     path: "/drinks",
     name: "Drinks",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Drinks.vue")
+
+    component: () => import("../views/Drinks.vue")
   }
 ];
 
--- src/views/Drinks.vue
+++ src/views/Drinks.vue
@@ -1,20 +1,68 @@
 <template>
-  <div class="search-page text-center mt-5">
+  <div class="search-page text-center w-75 ml-auto mr-auto mt-5">
     <h1>Cocktail search</h1>
     <p>
       Looking to kick off the weekend? Having a party with friends? Or maybe you
       just want to curl up with a good book and a great drink. Whatever the
       situation, you'll find the perfect cocktail at The Cocktail App. Search by
-      cocktail name, or first letter, or look up some random cocktails on the
+      cocktail name, or look up some random cocktails on the
       <b-link to="/">home</b-link> page.
     </p>
-    <div class="search-form"></div>
+    <div class="search-form">
+      <b-form inline @submit="onSubmit">
+        <label for="input-cocktail-name">Search for a cocktail: </label>
+        <b-form-input
+          id="input-cocktail-name"
+          v-model="searchTerm"
+          class="mx-2 w-75"
+        ></b-form-input>
+        <b-button type="submit" variant="info">Search</b-button>
+      </b-form>
+
+      <!-- TODO make this a nice list with cards and such -->
+
+      <div class="mt-5" v-if="resultsFound > 0">
+        <h1>We found {{ resultsFound }} cocktails</h1>
+        <ul>
+          <li v-for="drink in drinks" :key="drink.strDrink">
+            {{ drink.strDrink }}
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
-<style lang="scss">
-  .search-page {
-    width: 75%;
-    margin: auto;
+<script>
+import axios from "axios";
+export default {
+  name: "Drinks",
+
+  data() {
+    return {
+      searchTerm: null,
+      drinks: []
+    };
+  },
+
+  computed: {
+    resultsFound: function() {
+      return this.drinks.length;
+    }
+  },
+
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      axios
+        .get(
+          `https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=${this.searchTerm}`
+        )
+        .then(result => (this.drinks = result.data.drinks));
+      //maybe do some error handling here
+    }
   }
-</style>
+};
+</script>
+
+<style lang="scss"></style>
